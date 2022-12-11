1f58e6c3f8d629b9fa36c52e58bfdc43bad4985c
juyingguo <juying2050@sina.cn>
2022-12-11 10:41:43 +0800

    chapter1,add book request test

--- chapter1/src/main/java/com/blurooo/chapter1_2/controller/Hello.java
+++ chapter1/src/main/java/com/blurooo/chapter1_2/controller/Hello.java
@@ -1,6 +1,7 @@
 package com.blurooo.chapter1_2.controller;
 
 
+import com.blurooo.chapter1_2.bean.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,14 @@ public class Hello {
     @GetMapping("/")
     public String hello() {
         return "hello, spring security";
+    }
+    @GetMapping("/book")
+    public Book book() {
+        Book book = new Book();
+        book.setName("三国演义123");
+        book.setAuthor("罗贯中");
+        System.out.println("book print::" + book);
+        return book;
     }
 
 }
